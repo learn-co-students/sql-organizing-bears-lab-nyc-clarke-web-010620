f0be9a26425b8faaa05ce2a3b79b672f96a28c31
Granit <ghaxhaj@fordham.edu>
2020-01-16 14:16:34 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color,temperament,alive) 
+VALUES ("Mr.Chocolate",7,"Male","Brown","Aggresive",true);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) 
+VALUES ("Rowdy",7,"Male","Brown","Aggresive",true);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) 
+VALUES ("Tabitha",7,"Male","Brown","Aggresive",true);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) 
+VALUES ("Sergeant Brown",7,"Male","Brown","Aggresive",true);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) 
+VALUES ("Melissa",7,"Male","Brown","Aggresive",true);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) 
+VALUES ("Grinch",7,"Male","Brown","Aggresive",true);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) 
+VALUES ("Wendy",7,"Male","Brown","Aggresive",true);
+
+INSERT INTO bears (age, gender, color,temperament,alive) 
+VALUES (7,"Male","Brown","Aggresive",true);
